1431a718f8f962cd8ad5bc7222e7c434562421de
abhishek-pokhriyal <pokhi@coloredcow.com>
2022-06-24 02:53:33 +0530

    Create index.html using HTML Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
  const path = require('path');
  const Dotenv = require('dotenv-webpack');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
  module.exports = {
   mode: 'development',
@@ -7,6 +8,13 @@
     static: './dist',
   },
   plugins: [
-    new Dotenv()
-  ]
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      title: 'Just JS with webpack',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+   },
  };
